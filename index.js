457235e5e76febf2ee5862cddcec36ea257afc60
Ranjan Shrestha <ranjan@sharingapples.com>
2018-09-12 17:57:27 +0545

    Run jar from .dynamodb folder

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
-const spawn = require('child_process').spawn;
+const { spawn } = require('child_process');
 const path = require('path');
+const fs = require('fs');
 
-const root = path.resolve(__dirname, 'aws');
+const root = path.resolve(__dirname, '.dynamodb');
 const libFolder = path.resolve(root, 'DynamoDBLocal_lib');
 const jarFile = path.resolve(root, 'DynamoDBLocal.jar');
 
 module.exports = (port = 8000, dataPath, delayTransientStatuses) => {
+  // Make sure the jar files have been downloaded
+  if (!fs.existsSync(jarFile)) {
+    throw new Error('AWS DynamoDB local server is not available.');
+  }
+
   const args = [
     `-Djava.library.path-=${libFolder}`,
     '-jar', jarFile,
@@ -25,19 +31,12 @@ module.exports = (port = 8000, dataPath, delayTransientStatuses) => {
     args.push('-delayTransientStatuses');
   }
 
-  const dynamoDB = spawn('java', args);
-
-  dynamoDB.stdout.on('data', data => {
-    console.log(data.toString());
-  });
-
-  dynamoDB.stderr.on('data', data => {
-    // In case of an error report the error
-    console.error(data.toString());
+  const dynamoDB = spawn('java', args, {
+    stdio: ['inherit', 'inherit', 'inherit'],
   });
 
   // Return a method to kill the spawned process
   return () => {
     dynamoDB.kill();
-  }
+  };
 };
