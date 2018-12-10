1ce6f0798d639e58b3d8d9ad43bef1492f231bbe
Ranjan Shrestha <ranjan@sharingapples.com>
2018-12-10 13:56:58 +0545

    Remove shebang

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 const { spawn } = require('child_process');
 const path = require('path');
 const fs = require('fs');
--- run.js
+++ run.js
@@ -1,4 +1,3 @@
-#!/usr/bin/env node
 const process = require('process');
 const path = require('path');
 
